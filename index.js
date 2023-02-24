969d0797c287ccdafce409115ba04b346093c6e0
DewaSRY <sdewa6645@gmail.com>
2023-02-25 07:22:04 +0800

    oke

---
+++ index.js
@@ -0,0 +1,34 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const confirmPlaned = [];
+function ishabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (ishabitablePlanet(data)) {
+      confirmPlaned.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(confirmPlaned);
+    console.log(confirmPlaned.length, "was good enought");
+    for (planet of confirmPlaned) {
+      console.log(planet["kepler_name"]);
+    }
+    console.log("done");
+  });
